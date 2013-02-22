43b816acb5fb5bcd958d19fe52f4fab162585383
Ryan McGowan <ryan@ryanmcg.com>
2013-02-22 10:45:23 -0500

    Put export for ANDROID_SDK_HOME in android plugin.

--- plugins/android/android.plugin.zsh
+++ plugins/android/android.plugin.zsh
@@ -1,2 +1,3 @@
 # Add the android sdk tools to the path.
 export PATH=$HOME/repo/adt-bundle/sdk/tools:$HOME/repo/adt-bundle/sdk/platform-tools:$PATH
+export ANDROID_SDK_HOME=$HOME/repo/adt-bundle/sdk
