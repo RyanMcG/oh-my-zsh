7154903006aa4b144d4a431c7796f5c7104a0f20
Ryan McGowan <ryan@ryanmcg.com>
2013-07-11 16:47:48 -0700

    Remove ls aliases.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -15,12 +15,5 @@ alias please='sudo'
 # Show history
 alias history='fc -l 1'
 
-# List direcory contents
-alias lsa='ls -lah'
-alias l='ls -lA1'
-alias ll='ls -l'
-alias la='ls -lA'
-alias sl=ls # often screw this up
-
 alias afind='ack-grep -il'
 
